832c32ff0ee02065e27a585e659317e729c80493
JobCorte <job561.corte@gmail.com>
2021-02-16 11:56:04 -0600

    se agregaron 5 grupbox con sus nombre en espesifico al igual herramientas en cada una de ellas como abel, textbox, radiobutton, imagen, bombobox, checbox

---
+++ Examen/Examen/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace Examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpbxdatospersonales = new System.Windows.Forms.GroupBox();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.lblapellidopaterno = new System.Windows.Forms.Label();
+            this.lblapellidomaterno = new System.Windows.Forms.Label();
+            this.abltelefonofijo = new System.Windows.Forms.Label();
+            this.abltelefonomovil = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.grpbxgenero = new System.Windows.Forms.GroupBox();
+            this.grpbxtipo = new System.Windows.Forms.GroupBox();
+            this.rdbtnm = new System.Windows.Forms.RadioButton();
+            this.rdbntf = new System.Windows.Forms.RadioButton();
+            this.rdbntn = new System.Windows.Forms.RadioButton();
+            this.rdbtne = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grpbxfoto = new System.Windows.Forms.GroupBox();
+            this.grpbxdatoscarrera = new System.Windows.Forms.GroupBox();
+            this.lblcarrera = new System.Windows.Forms.Label();
+            this.lblsemestre = new System.Windows.Forms.Label();
+            this.lblpromedio = new System.Windows.Forms.Label();
+            this.chboxserviosocial = new System.Windows.Forms.CheckBox();
+            this.cmbboxcarreras = new System.Windows.Forms.ComboBox();
+            this.cmbbxsemestre = new System.Windows.Forms.ComboBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.grpbxdatospersonales.SuspendLayout();
+            this.grpbxgenero.SuspendLayout();
+            this.grpbxtipo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grpbxfoto.SuspendLayout();
+            this.grpbxdatoscarrera.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpbxdatospersonales
+            // 
+            this.grpbxdatospersonales.Controls.Add(this.textBox5);
+            this.grpbxdatospersonales.Controls.Add(this.textBox4);
+            this.grpbxdatospersonales.Controls.Add(this.textBox3);
+            this.grpbxdatospersonales.Controls.Add(this.textBox2);
+            this.grpbxdatospersonales.Controls.Add(this.textBox1);
+            this.grpbxdatospersonales.Controls.Add(this.lblapellidopaterno);
+            this.grpbxdatospersonales.Controls.Add(this.lblapellidomaterno);
+            this.grpbxdatospersonales.Controls.Add(this.abltelefonofijo);
+            this.grpbxdatospersonales.Controls.Add(this.abltelefonomovil);
+            this.grpbxdatospersonales.Controls.Add(this.lblnombre);
+            this.grpbxdatospersonales.Location = new System.Drawing.Point(12, 23);
+            this.grpbxdatospersonales.Name = "grpbxdatospersonales";
+            this.grpbxdatospersonales.Size = new System.Drawing.Size(523, 230);
+            this.grpbxdatospersonales.TabIndex = 0;
+            this.grpbxdatospersonales.TabStop = false;
+            this.grpbxdatospersonales.Text = "Datos personales";
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(6, 28);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(62, 17);
+            this.lblnombre.TabIndex = 1;
+            this.lblnombre.Text = "Nombre:";
+            // 
+            // lblapellidopaterno
+            // 
+            this.lblapellidopaterno.AutoSize = true;
+            this.lblapellidopaterno.Location = new System.Drawing.Point(6, 71);
+            this.lblapellidopaterno.Name = "lblapellidopaterno";
+            this.lblapellidopaterno.Size = new System.Drawing.Size(116, 17);
+            this.lblapellidopaterno.TabIndex = 2;
+            this.lblapellidopaterno.Text = "Apellido Paterno:";
+            // 
+            // lblapellidomaterno
+            // 
+            this.lblapellidomaterno.AutoSize = true;
+            this.lblapellidomaterno.Location = new System.Drawing.Point(6, 113);
+            this.lblapellidomaterno.Name = "lblapellidomaterno";
+            this.lblapellidomaterno.Size = new System.Drawing.Size(118, 17);
+            this.lblapellidomaterno.TabIndex = 3;
+            this.lblapellidomaterno.Text = "Apellido Materno:";
+            // 
+            // abltelefonofijo
+            // 
+            this.abltelefonofijo.AutoSize = true;
+            this.abltelefonofijo.Location = new System.Drawing.Point(6, 158);
+            this.abltelefonofijo.Name = "abltelefonofijo";
+            this.abltelefonofijo.Size = new System.Drawing.Size(94, 17);
+            this.abltelefonofijo.TabIndex = 4;
+            this.abltelefonofijo.Text = "Teléfono Fijo:";
+            // 
+            // abltelefonomovil
+            // 
+            this.abltelefonomovil.AutoSize = true;
+            this.abltelefonomovil.Location = new System.Drawing.Point(6, 200);
+            this.abltelefonomovil.Name = "abltelefonomovil";
+            this.abltelefonomovil.Size = new System.Drawing.Size(104, 17);
+            this.abltelefonomovil.TabIndex = 5;
+            this.abltelefonomovil.Text = "Teléfono Móvil:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(184, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(333, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(184, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(333, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(184, 153);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(333, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(184, 195);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(333, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // grpbxgenero
+            // 
+            this.grpbxgenero.Controls.Add(this.rdbntf);
+            this.grpbxgenero.Controls.Add(this.rdbtnm);
+            this.grpbxgenero.Location = new System.Drawing.Point(550, 21);
+            this.grpbxgenero.Name = "grpbxgenero";
+            this.grpbxgenero.Size = new System.Drawing.Size(72, 118);
+            this.grpbxgenero.TabIndex = 1;
+            this.grpbxgenero.TabStop = false;
+            this.grpbxgenero.Text = "Género";
+            // 
+            // grpbxtipo
+            // 
+            this.grpbxtipo.Controls.Add(this.rdbntn);
+            this.grpbxtipo.Controls.Add(this.rdbtne);
+            this.grpbxtipo.Location = new System.Drawing.Point(550, 145);
+            this.grpbxtipo.Name = "grpbxtipo";
+            this.grpbxtipo.Size = new System.Drawing.Size(72, 108);
+            this.grpbxtipo.TabIndex = 2;
+            this.grpbxtipo.TabStop = false;
+            this.grpbxtipo.Text = "Tipo";
+            // 
+            // rdbtnm
+            // 
+            this.rdbtnm.AutoSize = true;
+            this.rdbtnm.Location = new System.Drawing.Point(6, 26);
+            this.rdbtnm.Name = "rdbtnm";
+            this.rdbtnm.Size = new System.Drawing.Size(40, 21);
+            this.rdbtnm.TabIndex = 3;
+            this.rdbtnm.TabStop = true;
+            this.rdbtnm.Text = "M";
+            this.rdbtnm.UseVisualStyleBackColor = true;
+            // 
+            // rdbntf
+            // 
+            this.rdbntf.AutoSize = true;
+            this.rdbntf.Location = new System.Drawing.Point(6, 71);
+            this.rdbntf.Name = "rdbntf";
+            this.rdbntf.Size = new System.Drawing.Size(37, 21);
+            this.rdbntf.TabIndex = 4;
+            this.rdbntf.TabStop = true;
+            this.rdbntf.Text = "F";
+            this.rdbntf.UseVisualStyleBackColor = true;
+            // 
+            // rdbntn
+            // 
+            this.rdbntn.AutoSize = true;
+            this.rdbntn.Location = new System.Drawing.Point(6, 36);
+            this.rdbntn.Name = "rdbntn";
+            this.rdbntn.Size = new System.Drawing.Size(39, 21);
+            this.rdbntn.TabIndex = 5;
+            this.rdbntn.TabStop = true;
+            this.rdbntn.Text = "N";
+            this.rdbntn.UseVisualStyleBackColor = true;
+            // 
+            // rdbtne
+            // 
+            this.rdbtne.AutoSize = true;
+            this.rdbtne.Location = new System.Drawing.Point(6, 81);
+            this.rdbtne.Name = "rdbtne";
+            this.rdbtne.Size = new System.Drawing.Size(38, 21);
+            this.rdbtne.TabIndex = 6;
+            this.rdbtne.TabStop = true;
+            this.rdbtne.Text = "E";
+            this.rdbtne.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Examen.Properties.Resources.IMG_20160102_WA0031;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // grpbxfoto
+            // 
+            this.grpbxfoto.Controls.Add(this.pictureBox1);
+            this.grpbxfoto.Location = new System.Drawing.Point(635, 23);
+            this.grpbxfoto.Name = "grpbxfoto";
+            this.grpbxfoto.Size = new System.Drawing.Size(162, 224);
+            this.grpbxfoto.TabIndex = 4;
+            this.grpbxfoto.TabStop = false;
+            this.grpbxfoto.Text = "Fotografia";
+            // 
+            // grpbxdatoscarrera
+            // 
+            this.grpbxdatoscarrera.Controls.Add(this.textBox6);
+            this.grpbxdatoscarrera.Controls.Add(this.cmbbxsemestre);
+            this.grpbxdatoscarrera.Controls.Add(this.cmbboxcarreras);
+            this.grpbxdatoscarrera.Controls.Add(this.chboxserviosocial);
+            this.grpbxdatoscarrera.Controls.Add(this.lblsemestre);
+            this.grpbxdatoscarrera.Controls.Add(this.lblpromedio);
+            this.grpbxdatoscarrera.Controls.Add(this.lblcarrera);
+            this.grpbxdatoscarrera.Location = new System.Drawing.Point(12, 259);
+            this.grpbxdatoscarrera.Name = "grpbxdatoscarrera";
+            this.grpbxdatoscarrera.Size = new System.Drawing.Size(778, 130);
+            this.grpbxdatoscarrera.TabIndex = 5;
+            this.grpbxdatoscarrera.TabStop = false;
+            this.grpbxdatoscarrera.Text = "Datos carrera";
+            // 
+            // lblcarrera
+            // 
+            this.lblcarrera.AutoSize = true;
+            this.lblcarrera.Location = new System.Drawing.Point(6, 36);
+            this.lblcarrera.Name = "lblcarrera";
+            this.lblcarrera.Size = new System.Drawing.Size(60, 17);
+            this.lblcarrera.TabIndex = 6;
+            this.lblcarrera.Text = "Carrera:";
+            // 
+            // lblsemestre
+            // 
+            this.lblsemestre.AutoSize = true;
+            this.lblsemestre.Location = new System.Drawing.Point(6, 65);
+            this.lblsemestre.Name = "lblsemestre";
+            this.lblsemestre.Size = new System.Drawing.Size(72, 17);
+            this.lblsemestre.TabIndex = 7;
+            this.lblsemestre.Text = "Semestre:";
+            // 
+            // lblpromedio
+            // 
+            this.lblpromedio.AutoSize = true;
+            this.lblpromedio.Location = new System.Drawing.Point(6, 101);
+            this.lblpromedio.Name = "lblpromedio";
+            this.lblpromedio.Size = new System.Drawing.Size(72, 17);
+            this.lblpromedio.TabIndex = 8;
+            this.lblpromedio.Text = "Promedio:";
+            // 
+            // chboxserviosocial
+            // 
+            this.chboxserviosocial.AutoSize = true;
+            this.chboxserviosocial.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chboxserviosocial.Location = new System.Drawing.Point(588, 95);
+            this.chboxserviosocial.Name = "chboxserviosocial";
+            this.chboxserviosocial.Size = new System.Drawing.Size(184, 21);
+            this.chboxserviosocial.TabIndex = 6;
+            this.chboxserviosocial.Text = "Servicio social concluido";
+            this.chboxserviosocial.UseVisualStyleBackColor = true;
+            // 
+            // cmbboxcarreras
+            // 
+            this.cmbboxcarreras.FormattingEnabled = true;
+            this.cmbboxcarreras.Items.AddRange(new object[] {
+            "Ingenieria de software",
+            "ingenieria de IT",
+            "Mecatronica",
+            "Derecho",
+            "Economia"});
+            this.cmbboxcarreras.Location = new System.Drawing.Point(184, 33);
+            this.cmbboxcarreras.Name = "cmbboxcarreras";
+            this.cmbboxcarreras.Size = new System.Drawing.Size(588, 24);
+            this.cmbboxcarreras.TabIndex = 6;
+            // 
+            // cmbbxsemestre
+            // 
+            this.cmbbxsemestre.FormattingEnabled = true;
+            this.cmbbxsemestre.Items.AddRange(new object[] {
+            "Primero",
+            "Segundo",
+            "Tercero ",
+            "Cuarto",
+            "Quinto",
+            "Sexto"});
+            this.cmbbxsemestre.Location = new System.Drawing.Point(184, 65);
+            this.cmbbxsemestre.Name = "cmbbxsemestre";
+            this.cmbbxsemestre.Size = new System.Drawing.Size(588, 24);
+            this.cmbbxsemestre.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(184, 96);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(229, 22);
+            this.textBox6.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 775);
+            this.Controls.Add(this.grpbxdatoscarrera);
+            this.Controls.Add(this.grpbxfoto);
+            this.Controls.Add(this.grpbxtipo);
+            this.Controls.Add(this.grpbxgenero);
+            this.Controls.Add(this.grpbxdatospersonales);
+            this.Name = "Form1";
+            this.Text = "Asignación Práctica Profecional";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpbxdatospersonales.ResumeLayout(false);
+            this.grpbxdatospersonales.PerformLayout();
+            this.grpbxgenero.ResumeLayout(false);
+            this.grpbxgenero.PerformLayout();
+            this.grpbxtipo.ResumeLayout(false);
+            this.grpbxtipo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grpbxfoto.ResumeLayout(false);
+            this.grpbxdatoscarrera.ResumeLayout(false);
+            this.grpbxdatoscarrera.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpbxdatospersonales;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblapellidopaterno;
+        private System.Windows.Forms.Label lblapellidomaterno;
+        private System.Windows.Forms.Label abltelefonofijo;
+        private System.Windows.Forms.Label abltelefonomovil;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.GroupBox grpbxgenero;
+        private System.Windows.Forms.RadioButton rdbntf;
+        private System.Windows.Forms.RadioButton rdbtnm;
+        private System.Windows.Forms.GroupBox grpbxtipo;
+        private System.Windows.Forms.RadioButton rdbntn;
+        private System.Windows.Forms.RadioButton rdbtne;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox grpbxfoto;
+        private System.Windows.Forms.GroupBox grpbxdatoscarrera;
+        private System.Windows.Forms.ComboBox cmbboxcarreras;
+        private System.Windows.Forms.CheckBox chboxserviosocial;
+        private System.Windows.Forms.Label lblsemestre;
+        private System.Windows.Forms.Label lblpromedio;
+        private System.Windows.Forms.Label lblcarrera;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox cmbbxsemestre;
+    }
+}
+
